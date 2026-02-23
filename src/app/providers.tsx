26b73e811593478f47d85308854390837470b510
Dmitrii Frolov <dmitri.frolof@gmail.com>
2026-02-23 21:06:29 +0100

    dashboard page

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,5 +4,9 @@ import { ApolloProvider } from "@apollo/client/react";
 import { apolloClient } from "@/lib/apollo-client";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ApolloProvider client={apolloClient}>{children}</ApolloProvider>;
+  return (
+    <ApolloProvider client={apolloClient}>
+      {children}
+    </ApolloProvider>
+  );
 }
